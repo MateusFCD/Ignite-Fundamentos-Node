d8127b770c208bad9536ac4c56f6a524d4ed2ade
Mateus Drumond <mateusdrumond.dev@gmail.com>
2025-02-17 21:14:02 -0300

    update

--- src/utils/build-route-path.js
+++ src/utils/build-route-path.js
@@ -1,13 +1,13 @@
 // /user/:id
 
 export function buildRoutePath(path) {
-  const routeParametersRegex = /:([a-zA-Z]+)/g;
-  const pathWithParams = path.replaceAll(
-    routeParametersRegex,
-    "(?<$1>[a-z0-9-_]+)"
-  );
+    const routeParametersRegex = /:([a-zA-Z]+)/g;
+    const pathWithParams = path.replaceAll(
+        routeParametersRegex,
+        "(?<$1>[a-z0-9\-_]+)"
+    );
 
-  const pathReges = new RegExp(`^${pathWithParams}(?<query>\\?(.*))?$`);
+    const pathRegex = new RegExp(`^${pathWithParams}(?<query>\\?(.*))?$`);
 
-  return pathReges;
+    return pathRegex;
 }
